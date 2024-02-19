7e1cff4c59d53c3b4bdcd5848449fa8ad1d87167
rijfe <kyw42510@naver.com>
2024-02-19 09:03:59 +0900

    R3F Event 실습

--- chaper03-r3f/src/components/GLBModels.jsx
+++ chaper03-r3f/src/components/GLBModels.jsx
@@ -1,6 +1,6 @@
 import { useAnimations, useGLTF } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
 function GLBModels(){
@@ -8,6 +8,7 @@ function GLBModels(){
     const ref = useRef(null);
     const three = useThree(); // 아래 useFrame과 같은 객체를 사용할 수 있음.
     const {actions} = useAnimations(animations, ref);
+    const [ani, setAni] = useState("wave");
 
     useEffect(()=>{
         scene.traverse(obj=>{
@@ -16,15 +17,32 @@ function GLBModels(){
                 obj.receiveShadow = true;
             }
         });
-        actions["wave"].play();
     },[scene,actions]);
 
-    useFrame((state, delta)=>{
+    useEffect(()=>{
+        actions[ani].fadeIn(0.5).play();
+        return(()=>{
+            actions[ani].fadeOut(0.5).stop();
+        })
+    },[actions, ani]);
 
-    });
+    useFrame((state, delta)=>{});
 
     return (
-        <primitive ref={ref} object={scene} scale={0.01} position-y={0.8}/>
+        <primitive 
+            ref={ref} 
+            object={scene} 
+            scale={0.01} 
+            position-y={0.8}
+            onClick={()=>{
+                setAni(prev=>{
+                    if(prev === "wave") return "windmill";
+                    return "wave";
+                });
+            }}
+            onPointerUp={()=>{}}
+            onPointerDown={()=>{}}
+        />
     );
 }
 
